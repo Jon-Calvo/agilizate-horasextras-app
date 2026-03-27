3cea1f6b18214691ad70b84310a1a1b1b0d6e1e3
Jon-Calvo <jonathan.j.calvo@gmail.com>
2026-03-26 23:11:35 -0300

    Update config.js
    Versión inicial

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/TU_URL_DEPLOYMENT_AQUI/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxJ-vMgNSpcZR9aNz_jWtw2rnAE5qUk24k8eBjR0H-djRJuTpLvKem36KubANfb6xwS/exec',
   APP_NAME: 'AGILIZATE',
   VERSION: '1.0.0',
   STORAGE_KEYS: {
